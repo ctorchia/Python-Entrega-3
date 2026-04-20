9d2bcb780ecd81e0e01f4832b004e7960934d0c9
DESKTOP-V2BVTP7\CMT <cristian_torchia@yahoo.com.ar>
2026-04-20 20:45:00 -0300

    Agregando app de cuentas 001

--- maraartesanias/settings.py
+++ maraartesanias/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'clientes',
     'proveedores',
     'productos',
+    'cuentas',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
+AUTH_USER_MODEL = "cuentas.Perfil"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "perfil_detail"
+LOGOUT_REDIRECT_URL = "login"
